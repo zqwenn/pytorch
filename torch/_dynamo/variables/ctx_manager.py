9792b830c613b77888fddd034c143ea36eef41b6
Brian Hirsh <hirsheybar@fb.com>
2024-10-16 13:28:08 -0700

    update _unsafe_set_version_counter to accept lists of tensors
    ghstack-source-id: 5c0ce800dacffead653bda66d6d1f4385c4c8deb
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/137921

--- torch/_dynamo/variables/ctx_manager.py
+++ torch/_dynamo/variables/ctx_manager.py
@@ -917,11 +917,18 @@ class PreserveVersionContextVariable(ContextWrappingVariable):
             )
         )
 
-    def __init__(self, tensor, prev_version, **kwargs) -> None:
+    def __init__(self, tensors, prev_versions, **kwargs) -> None:
         kwargs.setdefault("target_values", None)
         super().__init__(**kwargs)
-        self.tensor = tensor
-        self.prev_version = prev_version
+        self.tensors = tensors
+        self.prev_versions = prev_versions
+        # The context manager accepts Union[Tensor, Tuple[Tensor]]
+        if isinstance(self.tensors, variables.TensorVariable):
+            self.tensors = variables.TupleVariable([self.tensors])
+        if isinstance(
+            self.prev_versions, (variables.ConstantVariable, variables.SymNodeVariable)
+        ):
+            self.prev_versions = variables.TupleVariable([self.prev_versions])
 
     def enter(self, tx):
         pass
@@ -931,7 +938,7 @@ class PreserveVersionContextVariable(ContextWrappingVariable):
 
         return variables.TorchInGraphFunctionVariable(
             _unsafe_set_version_counter
-        ).call_function(tx, [self.tensor, self.prev_version], {})
+        ).call_function(tx, [self.tensors, self.prev_versions], {})
 
     def reconstruct(self, codegen):
         unimplemented(
