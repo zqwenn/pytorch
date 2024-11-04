aa5f64679ea5bd9de8c9cb6bdb3d88b4da243e1f
Will Feng <yf225@cornell.edu>
2024-11-04 12:24:35 -0800

    [WIP] raise FSDP2 backward all-gather in aot_inference graph; force reshard before unshard in pre-backward hook
    ghstack-source-id: 5844398de22d0d7736f0ba0b981a43b0533b0e24
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/139535

--- torch/distributed/_composable/fsdp/_fsdp_param_group.py
+++ torch/distributed/_composable/fsdp/_fsdp_param_group.py
@@ -2,6 +2,7 @@
 import contextlib
 import logging
 from typing import Any, Callable, cast, Dict, List, NamedTuple, Optional, Set, Tuple
+import functools
 
 import torch
 import torch.distributed as dist
@@ -345,6 +346,10 @@ class FSDPParamGroup:
         self._post_forward_indices.append(post_forward_index)
 
     def pre_backward(self, default_prefetch: bool, *unused: Any):
+        if compiled_autograd_enabled() and self._training_state == TrainingState.PRE_BACKWARD:
+            # Traceable FSDP2 cannot trigger the param group's post-backward immediately after param usage;
+            # instead it relies on this to trigger the previously pending post-backward.
+            self.post_backward()
         if self._training_state == TrainingState.PRE_BACKWARD:
             return
         if not compiled_autograd_enabled():
@@ -677,8 +682,8 @@ class RegisterPostBackwardFunction(torch.autograd.Function):
         if compiled_autograd_enabled():
             # TODO: Find a way to print the offending FSDP2 module.
             msg = """\
-When Traceable FSDP2 is enabled, we rely on `root_post_backward_callback` to call
-each `FSDPParamGroup.post_backward`, and we should not be calling into `RegisterPostBackwardFunction`.
+When Traceable FSDP2 is enabled, we rely on the param group's next `pre_backward` hook or `root_post_backward_callback`
+to trigger the previous `post_backward` hook, and we should not be calling into `RegisterPostBackwardFunction`.
 If you are here, it means the forward part of this FSDP2 instance is not compiled, and you must also
 compile the forward part if you want to use Traceable FSDP2."""
             torch._dynamo.comptime.comptime.print(msg)
