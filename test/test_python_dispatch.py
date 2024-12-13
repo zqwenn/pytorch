91cd09bcf2446ea4b8d4726f37872ab9a467f938
rzou <zou3519@gmail.com>
2024-12-13 08:17:10 -0800

    Allow Nones in Tensor[] for IValues
    When we parse IValues from Python to C++, we treat Nones as Tensors
    (these get parsed into undefined tensors). Before this PR, this logic
    only happened for Tensor (and not Tensor[]). For example, we could
    create a custom operator foo(Tensor x) and it would accept None as
    input.
    
    This PR makes the behavior consistent by extending it to Tensor[].
    
    I need this behavior later on when parsing lists of grad_output which
    look like they are List[Tensor] in C++ but become List[Optional[Tensor]]
    in Python (because undefined tensor in C++ maps to Python None).
    
    Test Plan:
    - new test
    
    ghstack-source-id: 2b4a4a4d3b270962b2a7429d45a82c83ff6efd66
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/143168

--- test/test_python_dispatch.py
+++ test/test_python_dispatch.py
@@ -570,6 +570,19 @@ class TestPythonRegistration(TestCase):
             with self.assertRaisesRegex(ValueError, "reserved namespace"):
                 my_lib1 = Library("prim", kind)  # noqa: TOR901
 
+    def test_undefined_tensors(self):
+        with torch.library._scoped_library(self.test_ns, "FRAGMENT") as lib:
+            lib.define("foos(Tensor[] ts) -> Tensor[]")
+
+            def foo_impl(ts):
+                return [t.clone() if t is not None else None for t in ts]
+
+            lib.impl("foos", foo_impl, "CompositeImplicitAutograd")
+
+            ts = [None, torch.randn(3), None, torch.randn(4)]
+            results = getattr(torch.ops, self.test_ns).foos(ts)
+            self.assertEqual(results, ts)
+
     def test_returning_symint(self) -> None:
         shape_env = ShapeEnv()
         fake_tensor_mode = FakeTensorMode(shape_env=shape_env)
