3ffef81ed4901fb2ad07b1dfac642e25e4ef2cab
Avik Chaudhuri <avik@fb.com>
2025-01-14 11:36:25 -0800

    fix as_bool serde
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/144791
    
    
    ghstack-source-id: 261383465
    @exported-using-ghexport
    
    Differential Revision: [D68167701](https://our.internmc.facebook.com/intern/diff/D68167701/)

--- test/export/test_export.py
+++ test/export/test_export.py
@@ -2084,8 +2084,6 @@ graph():
             with torch._functorch.config.patch(fake_tensor_propagate_real_tensors=True):
                 ep = export(model, inputs)
 
-    @testing.expectedFailureSerDer  # SymBool serialization? TODO(pianpwk)
-    @testing.expectedFailureSerDerNonStrict
     def test_real_tensor_bool_cast(self):
         class Foo(torch.nn.Module):
             def forward(self, x):
@@ -2096,8 +2094,6 @@ graph():
         with torch._functorch.config.patch(fake_tensor_propagate_real_tensors=True):
             ep = export(model, inputs, strict=False)
 
-    @testing.expectedFailureSerDer
-    @testing.expectedFailureSerDerNonStrict
     def test_is_nonzero(self):
         class Foo(torch.nn.Module):
             def forward(self, x):
