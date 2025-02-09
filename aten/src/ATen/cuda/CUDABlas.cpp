fc21fa5a57984039f165809af5482d2be9457e75
drisspg <drisspguessous@gmail.com>
2025-02-09 14:57:23 -0800

    torch._scaled_mm with MX dtypes
    TODO write me
    
    ghstack-source-id: 7b0e0830e61676450d4868c61dd9f7c4558c0ccb
    ghstack-comment-id: 2641410829
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/146655

--- aten/src/ATen/cuda/CUDABlas.cpp
+++ aten/src/ATen/cuda/CUDABlas.cpp
@@ -13,6 +13,8 @@
 #include <c10/macros/Export.h>
 #include <c10/util/env.h>
 #include <c10/util/irange.h>
+#include <c10/core/Allocator.h>
+#include <c10/core/ScalarType.h>
 
 #ifdef USE_ROCM
 #include <hipblaslt/hipblaslt-ext.hpp>
@@ -1422,10 +1424,12 @@ void scaled_gemm(
     const void* mat1_scale_ptr,
     int64_t mat1_ld,
     ScalarType mat1_dtype,
+    ScalarType mat1_scale_dtype,
     const void* mat2_ptr,
     const void* mat2_scale_ptr,
     int64_t mat2_ld,
     ScalarType mat2_dtype,
+    ScalarType mat2_scale_dtype,
     const void* bias_ptr,
     ScalarType bias_dtype,
     void* result_ptr,
@@ -1478,6 +1482,16 @@ void scaled_gemm(
     computeDesc.setAttribute(CUBLASLT_MATMUL_DESC_EPILOGUE, CUBLASLT_EPILOGUE_BIAS);
     computeDesc.setAttribute(CUBLASLT_MATMUL_DESC_BIAS_DATA_TYPE, ScalarTypeToCudaDataType(bias_dtype));
   }
+
+  if (mat1_scale_dtype == kFloat8_e8m0fnu && mat2_scale_dtype == kFloat8_e8m0fnu) {
+#if CUDA_VERSION >= 12000
+    computeDesc.setAttribute(CUBLASLT_MATMUL_DESC_A_SCALE_MODE, CUBLASLT_MATMUL_MATRIX_SCALE_VEC32_UE8M0);
+    computeDesc.setAttribute(CUBLASLT_MATMUL_DESC_B_SCALE_MODE, CUBLASLT_MATMUL_MATRIX_SCALE_VEC32_UE8M0);
+#else
+    TORCH_CHECK(false, "scaled_gemm with `torch.float8_e8m0fnu` scales is only supported for CUDA 12.0 and above");
+#endif // CUDA_VERSION >= 12000
+  }
+
   size_t workspaceSize = _getWorkspaceSize();
   auto workspace = at::empty(static_cast<int64_t>(workspaceSize), at::TensorOptions().dtype(at::kByte).device(at::kCUDA));
 
--- aten/src/ATen/native/cuda/Blas.cpp
+++ aten/src/ATen/native/cuda/Blas.cpp
@@ -1,4 +1,5 @@
 #include <cstdint>
+#include <c10/util/typeid.h>
 #include <c10/util/Exception.h>
 #include <c10/core/Scalar.h>
 #include <c10/core/ScalarType.h>
@@ -892,9 +893,10 @@ static bool _scaled_mm_is_fnuz() {
 
 namespace{
 
-enum class ScalingType {
+enum class ScalingType : std::uint8_t {
   TensorWise,
   RowWise,
+  BlockWise,
   Error
 };
 /*
@@ -919,6 +921,10 @@ ScalingType get_scaling_type(
     const at::Tensor& scale_b,
     int64_t dim_m,
     int64_t dim_n) {
+  // TODO(before land): clean up
+  if (scale_a.scalar_type() == scale_b.scalar_type() && scale_a.scalar_type() == at::kFloat8_e8m0fnu) {
+    return ScalingType::BlockWise;
+  }
   // Both Per-Tensor and Row-wise scaling expect fp32 tensors
   TORCH_CHECK(
       scale_a.scalar_type() == kFloat && scale_b.scalar_type() == kFloat,
@@ -1202,10 +1208,12 @@ _scaled_mm_out_cuda(const Tensor& mat1, const Tensor& mat2,
         scale_a.data_ptr(),
         args.lda,
         args.mata->scalar_type(),
+        scale_a.scalar_type(),
         args.matb->data_ptr(),
         scale_b.data_ptr(),
         args.ldb,
         args.matb->scalar_type(),
+        scale_b.scalar_type(),
         bias ? bias->data_ptr(): nullptr,
         bias ? bias->scalar_type() : isFloat8Type(out_dtype_) ? at::ScalarType::Half : out_dtype_,
         args.result->data_ptr(),
