8d9700d9f5a29077493c9ba9f5bc56537e61ff72
Yan Zhiwei <zhiwei.yan@intel.com>
2025-02-09 23:37:24 -0800

    [Intel GPU] qlinear.pointwise with mixed dtype support
    ghstack-source-id: 9f6d3d3ba22e9c3138eb9dbb2043711db6563cc9
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/136753

--- aten/src/ATen/native/mkldnn/xpu/qlinear.cpp
+++ aten/src/ATen/native/mkldnn/xpu/qlinear.cpp
@@ -37,9 +37,14 @@ Tensor q_linear_pointwise(
 
   std::vector<int64_t> src_dims = {M, K};
   std::vector<int64_t> dst_dims = {M, N};
-  auto out_dtype =
-      output_dtype.has_value() ? output_dtype.value() : act.scalar_type();
-  Tensor qout = at::empty(dst_dims, act.options().dtype(out_dtype));
+
+  bool fp32_output = output_dtype.has_value() && (output_dtype == c10::kFloat);
+  bool bfloat16_output =
+      output_dtype.has_value() && (output_dtype == c10::kBFloat16);
+  auto dst_dtype = fp32_output
+      ? c10::kFloat
+      : (bfloat16_output ? c10::kBFloat16 : act.scalar_type());
+  Tensor qout = at::empty(dst_dims, act.options().dtype(dst_dtype));
 
   quantized_matmul(
       act.contiguous(),
@@ -96,9 +101,14 @@ Tensor q_linear_pointwise_tensor(
 
   std::vector<int64_t> src_dims = {M, K};
   std::vector<int64_t> dst_dims = {M, N};
-  auto out_dtype =
-      output_dtype.has_value() ? output_dtype.value() : act.scalar_type();
-  Tensor qout = at::empty(dst_dims, act.options().dtype(out_dtype));
+
+  bool fp32_output = output_dtype.has_value() && (output_dtype == c10::kFloat);
+  bool bfloat16_output =
+      output_dtype.has_value() && (output_dtype == c10::kBFloat16);
+  auto dst_dtype = fp32_output
+      ? c10::kFloat
+      : (bfloat16_output ? c10::kBFloat16 : act.scalar_type());
+  Tensor qout = at::empty(dst_dims, act.options().dtype(dst_dtype));
 
   quantized_matmul(
       act.contiguous(),
@@ -154,9 +164,13 @@ Tensor q_linear_pointwise_binary(
 
   std::vector<int64_t> src_dims = {M, K};
   std::vector<int64_t> dst_dims = {M, N};
-  auto out_dtype =
-      output_dtype.has_value() ? output_dtype.value() : act.scalar_type();
-  Tensor qout = at::empty(dst_dims, device(c10::kXPU).dtype(out_dtype));
+  bool fp32_output = output_dtype.has_value() && (output_dtype == c10::kFloat);
+  bool bfloat16_output =
+      output_dtype.has_value() && (output_dtype == c10::kBFloat16);
+  auto dst_dtype = fp32_output
+      ? c10::kFloat
+      : (bfloat16_output ? c10::kBFloat16 : act.scalar_type());
+  Tensor qout = at::empty(dst_dims, device(c10::kXPU).dtype(dst_dtype));
 
   quantized_matmul(
       act.contiguous(),
@@ -211,9 +225,13 @@ Tensor q_linear_pointwise_binary_tensor(
 
   std::vector<int64_t> src_dims = {M, K};
   std::vector<int64_t> dst_dims = {M, N};
-  auto out_dtype =
-      output_dtype.has_value() ? output_dtype.value() : act.scalar_type();
-  Tensor qout = at::empty(dst_dims, device(c10::kXPU).dtype(out_dtype));
+  bool fp32_output = output_dtype.has_value() && (output_dtype == c10::kFloat);
+  bool bfloat16_output =
+      output_dtype.has_value() && (output_dtype == c10::kBFloat16);
+  auto dst_dtype = fp32_output
+      ? c10::kFloat
+      : (bfloat16_output ? c10::kBFloat16 : act.scalar_type());
+  Tensor qout = at::empty(dst_dims, device(c10::kXPU).dtype(dst_dtype));
 
   quantized_matmul(
       act.contiguous(),
