713dd4a47cfaeb9c798a8dadc8b0354b6f8b66cc
Animesh Jain <anijain@umich.edu>
2025-02-09 17:56:29 -0800

    [dynamo][not ready] polyfill infra for classes
    ghstack-source-id: a832221940e4f30a304897598b8f9a1fa0f50840
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/146678

--- torch/_dynamo/variables/__init__.py
+++ torch/_dynamo/variables/__init__.py
@@ -118,6 +118,7 @@ from .tensor import (
 from .torch import TorchCtxManagerClassVariable, TorchInGraphFunctionVariable
 from .user_defined import (
     MutableMappingVariable,
+    PolyFilledUserDefinedClassVariable,
     RemovableHandleVariable,
     UserDefinedClassVariable,
     UserDefinedDictVariable,
